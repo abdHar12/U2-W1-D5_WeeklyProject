811c3e90072e16a2d92df3139d1b2fae7498437f
abdHar12 <145681190+abdHar12@users.noreply.github.com>
2023-10-27 16:57:52 +0200

    First commit

---
+++ scr.js
@@ -0,0 +1,42 @@
+const elementToAnimate = document.querySelector("nav");
+const button = document.querySelector("#div-title span");
+const buttonToAnimate = document.querySelector("nav li:nth-of-type(5) > span");
+const elementToFix = document.querySelector("aside");
+const content = document.querySelector("#div-trending-on-medium ");
+// let h,
+//   de = document.documentElement;
+
+// if (self.innerHeight) {
+//   h = window.innerHeight;
+// } else if (de && de.clientHeight) {
+//   h = de.clientHeight;
+// } else if (document.body) {
+//   h = document.body.clientHeight;
+// }
+// elementToFix.style.height =
+//   toString(h - content.getBoundingClientRect().bottom) + "px";
+// console.log(elementToFix.style.height);
+window.addEventListener("scroll", () => {
+  const elementBottom = elementToAnimate.getBoundingClientRect().bottom;
+  const buttonBottom = button.getBoundingClientRect().bottom;
+  //   const h = document.body.offsetHeight;
+  //   const scrollPosition = window.scrollY;
+
+  if (scrollPosition > content.getBoundingClientRect().bottom) {
+    // elementToFix.classList.add("fixed");
+    elementToFix.style.top =
+      toString(h - content.getBoundingClientRect().bottom) + "px";
+  } else {
+    elementToFix.classList.remove("fixed");
+    elementToFix.style.top = "auto";
+  }
+
+  if (elementBottom > buttonBottom) {
+    elementToAnimate.classList.add("nav-animated");
+    buttonToAnimate.classList.add("button-animated");
+  }
+  if (elementBottom < buttonBottom) {
+    elementToAnimate.classList.remove("nav-animated");
+    buttonToAnimate.classList.remove("button-animated");
+  }
+});
